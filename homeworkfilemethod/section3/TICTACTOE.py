1b339643183d7b44ff7293aa51d6984c4dd3d6a2
avyan-singh <avyan.ssingh@gmail.com>
2025-06-16 12:08:09 +0530

    fixed code not same place

--- homeworkfilemethod/section3/TICTACTOE.py
+++ homeworkfilemethod/section3/TICTACTOE.py
@@ -61,20 +61,31 @@ def user_input(ranged, count, inputs, symbol1, symbol2):
             if(count%2==0):
                 place=int(input('Player1:Where you want to start with choose from 0,1,2,3,4,5,6,7,8 you can understand places from above numbering: '))
                 in_range(ranged, count, place)
+                if inputs[place] == ' ':
+                    inputs[place]=symbol1
+                    display_board(inputs)
+                else:
+                    while inputs[place]!=' ':
+                        place=int(input('Player1:Where you want to start with choose from 0,1,2,3,4,5,6,7,8 you can understand places from above numbering: '))
+                        in_range(ranged, count, place)
                 inputs[place]=symbol1
                 display_board(inputs)
-        
                 if inputs[0]==inputs[1]==inputs[2]==symbol1 or inputs[3]==inputs[4]==inputs[5]==symbol1 or inputs[6]==inputs[7]==inputs[8]==symbol1 or inputs[0]==inputs[3]==inputs[6]==symbol1 or inputs[1]==inputs[4]==inputs[7]==symbol1 or inputs[2]==inputs[5]==inputs[8]==symbol1 or inputs[0]==inputs[4]==inputs[8]==symbol1 or inputs[2]==inputs[4]==inputs[6]==symbol1:
                     print('Congratulations you won the game')
                     wongame=True
             else:
-                place=int(input('Player2:Where you want to start with choose from 0,1,2,3,4,5,6,7,8 you can understand places from above numbering: '))
-                in_range(ranged, count, place)
-                inputs[place]=symbol2
-                display_board(inputs)
+                while inputs[place]!=' ':
+                    place=int(input('Player2:Where you want to start with choose from 0,1,2,3,4,5,6,7,8 you can understand places from above numbering: '))
+                    in_range(ranged, count, place)
+                if inputs[place]==' ':
+                    inputs[place]=symbol2
+                    display_board(inputs)
+                else:
+                    while inputs[place]!=' ':
+                        place=int(input('Player2:Where you want to start with choose from 0,1,2,3,4,5,6,7,8 you can understand places from above numbering: '))
+                        in_range(ranged, count, place)
                 if inputs[0]==inputs[1]==inputs[2]==symbol2 or inputs[3]==inputs[4]==inputs[5]==symbol2 or inputs[6]==inputs[7]==inputs[8]==symbol2 or inputs[0]==inputs[3]==inputs[6]==symbol2 or inputs[1]==inputs[4]==inputs[7]==symbol2 or inputs[2]==inputs[5]==inputs[8]==symbol2 or inputs[0]==inputs[4]==inputs[8]==symbol2 or inputs[2]==inputs[4]==inputs[6]==symbol2:
                     print('Congratulations you won the game')
                     wongame=True
             count+=1
-# rhubXSFVDSXJNWEDSXGZBVF
 add_to_board()
