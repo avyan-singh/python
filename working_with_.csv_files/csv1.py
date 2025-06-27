90f6a70cee1bc68d4637c50ab69432c5f6fd891d
avyan-singh <avyan.ssingh@gmail.com>
2025-06-27 13:13:17 +0530

    increased performance

--- working_with_.csv_files/csv1.py
+++ working_with_.csv_files/csv1.py
@@ -1,20 +1,16 @@
 import csv
 emails=[]
-num=0
 data=open("C:\\avyan\\python\\working_with_.csv_files\\example.csv", encoding='utf-8')
 csv_data=csv.reader(data)
 data_lines=list(csv_data)
 for line in data_lines:
-    emails.append(data_lines[num][3])
-    num+=1  
+    emails.append(line[3])
 print(emails)#emails
 
 # full names
 full_names=[]
-num=0
 for line in data_lines:
-    full_names.append(data_lines[num][1] + " " + data_lines[num][2])
-    num+=1
+    full_names.append(line[1] + " " + line[2])
 print(full_names) # full names
 csv_file=open("C:\\avyan\\python\\working_with_.csv_files\\file1.csv", mode="w",newline='')
 csv_writer=csv.writer(csv_file,delimiter=',')
